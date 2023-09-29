cc7e1a2b08c91bd52973eab33c6e43375edd1b02
Alex Kaiser <kaiseale@mail.gvsu.edu>
2023-09-29 14:25:53 -0400

    Adds dynamoDB context and basic communication to database

--- src/API/Controllers/WeatherForecastController.cs
+++ src/API/Controllers/WeatherForecastController.cs
@@ -1,3 +1,5 @@
+using Amazon.DynamoDBv2.DataModel;
+using API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -12,22 +14,52 @@ namespace API.Controllers
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        private IDynamoDBContext _dynamoDbContext;
+        public WeatherForecastController(
+            IDynamoDBContext dynamoDBContext,
+            ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
+            _dynamoDbContext = dynamoDBContext;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<IEnumerable<WeatherForecast>> Get(string city = "Grand Rapids")
         {
+            return await _dynamoDbContext
+                .QueryAsync<WeatherForecast>(city)
+                .GetRemainingAsync();
+        }
+
+        [HttpPost(Name = "PostWeatherForecast")]
+        public async Task Post(string city = "GrandRapids")
+        {
+            var data = GenererateDummyData(city);
+            foreach (var d in data)
+            {
+                await _dynamoDbContext.SaveAsync(d);
+            }
+        }
+
+        private static WeatherForecast[] GenererateDummyData(string city)
+        {
+            var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                City = city,
+                Date = DateTime.UtcNow.Date.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpDelete(Name = "DeleteWeatherForecast")]
+        public async Task Delete(string city)
+        {
+
+            await _dynamoDbContext
+                .DeleteAsync<WeatherForecast>(city, DateTime.Now.Date);
         }
     }
 }
