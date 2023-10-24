87da587cc8c6b9e10838d7b1aeea5e2e92c235c7
Alex Kaiser <kaiseale@mail.gvsu.edu>
2023-10-24 13:20:11 -0400

    Defines models based on data available from external API

--- src/Backend/Models/GameScore.cs
+++ src/Backend/Models/GameScore.cs
@@ -1,20 +1,34 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-
 namespace Backend.Models
 {
-    internal class GameBet
+    public class Score
+    {
+        public string name { get; set; }
+        public int score { get; set; }
+
+    }
+    internal class GameScore
     {
         [Required]
         public string id { get; set; }
+
+        public string sport_key { get; set; }
 
         public string sport_title { get; set; }
 
         public string commence_time { get; set; }
 
+        public bool completed { get; set; }
+
         public string home_team { get; set; }
 
         public string away_team { get; set; }
+
+        public Score[] scores { get; set; }
+
+        public string last_update { get; set; }
+
 
     }
 }
