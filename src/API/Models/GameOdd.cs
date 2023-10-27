09ce6c687619b7c5c83a5c580fa1f495cb0a4323
Alex Kaiser <kaiseale@mail.gvsu.edu>
2023-10-27 11:33:35 -0400

    Adds game score and odd models

---
+++ src/API/Models/GameOdd.cs
@@ -0,0 +1,54 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class Outcome
+    {
+        public string name { get; set; }
+        public float price { get; set; }
+        public float? point { get; set; }
+    }
+
+    public class Market
+    {
+        [Required]
+        public string key { get; set; }
+        public string last_update { get; set; }
+
+        public Outcome[] outcomes { get; set; }
+
+    }
+    public class BookMaker
+    {
+        [Required]
+        public string key { get; set; }
+
+        public string title { get; set; }
+
+        public string last_update { get; set; }
+
+        public Market[] markets { get; set; }
+    }
+
+    [DynamoDBTable("GameOdds")]
+    public class GameOdd
+    {
+        [Required]
+        public string id { get; set; }
+
+        public string sport_key { get; set; }
+
+        public string sport_title { get; set; }
+
+        public string commence_time { get; set; }
+
+        public string home_team { get; set; }
+
+        public string away_team { get; set; }
+
+        public BookMaker[] bookmakers { get; set; }
+
+    }
+
+}
