390dbe5d6924b59565ebcd14b118cd23030083a8
Alex Kaiser <kaiseale@mail.gvsu.edu>
2023-10-30 16:58:41 -0400

    Adds API endpoints to get data by date

--- src/API/Controllers/GameOddController.cs
+++ src/API/Controllers/GameOddController.cs
@@ -1,6 +1,12 @@
-﻿using Amazon.DynamoDBv2.DataModel;
+﻿using Amazon.Auth.AccessControlPolicy;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Util;
 using API.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Xml.Linq;
 
 namespace API.Controllers
 {
@@ -10,7 +16,7 @@ namespace API.Controllers
     {
 
         private IDynamoDBContext _dbContext;
-        public GameOddController(IDynamoDBContext dynamoDBContext) 
+        public GameOddController(IDynamoDBContext dynamoDBContext)
         {
             _dbContext = dynamoDBContext;
         }
@@ -18,10 +24,54 @@ namespace API.Controllers
         [HttpGet]
         public async Task<IEnumerable<GameOdd>> GetAll()
         {
-            IEnumerable<ScanCondition> conditions = Enumerable.Empty<ScanCondition>();
+
+            List<ScanCondition> conditions = new List<ScanCondition>();
             var scan = await _dbContext.ScanAsync<GameOdd>(conditions).GetRemainingAsync();
             return scan;
         }
+
+        private IEnumerable<ScanCondition> _getConditions(string beginDate, string endDate = null)
+        {
+
+            string end = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
+            string begin = DateTime.Parse(beginDate).ToString("yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
+            List<ScanCondition> conditions = new List<ScanCondition>();
+
+            if (endDate != null)
+            {
+                /** return a range of dates **/
+
+                /** Add time to end day to include all games within that day **/
+                DateTime tmp = DateTime.Parse(endDate).AddHours(23).AddMinutes(59);
+                end = tmp.ToString("yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
+                conditions.Add(new ScanCondition("commence_time", ScanOperator.Between, begin, end));
+            }
+            else
+            {
+                /** return a single day **/
+                conditions.Add(new ScanCondition("commence_time", ScanOperator.BeginsWith, beginDate));
+            }
+
+            return conditions;
+
+
+        }
+
+        [HttpGet]
+        [Route("date", Name = "GetOddsByDate")]
+        public async Task<IEnumerable<GameOdd>> GetByDate(string begin, string? end = null)
+        {
+            IEnumerable<ScanCondition> conditions = _getConditions(begin, end);
+            DynamoDBOperationConfig config = new DynamoDBOperationConfig()
+            {
+                ConditionalOperator = ConditionalOperatorValues.And
+            };
+            var scan = await _dbContext.ScanAsync<GameOdd>(conditions, config).GetRemainingAsync();
+            return scan;
+        }
+
+
+        
 
         [HttpGet]
         [Route("{id}")]
--- src/API/Controllers/GameScoreController.cs
+++ src/API/Controllers/GameScoreController.cs
@@ -1,6 +1,8 @@
 using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
 using API.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 
 namespace API.Controllers
 {
@@ -24,6 +26,46 @@ namespace API.Controllers
         {
             IEnumerable<ScanCondition> conditions = Enumerable.Empty<ScanCondition>();
             var scan = await _dbContext.ScanAsync<GameScore>(conditions).GetRemainingAsync();
+            return scan;
+        }
+
+        private IEnumerable<ScanCondition> _getConditions(string beginDate, string endDate = null)
+        {
+
+            string end = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
+            string begin = DateTime.Parse(beginDate).ToString("yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
+            List<ScanCondition> conditions = new List<ScanCondition>();
+
+            if (endDate != null)
+            {
+                /** return a range of dates **/
+
+                /** Add time to end day to include all games within that day **/
+                DateTime tmp = DateTime.Parse(endDate).AddHours(23).AddMinutes(59);
+                end = tmp.ToString("yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
+                conditions.Add(new ScanCondition("commence_time", ScanOperator.Between, begin, end));
+            }
+            else
+            {
+                /** return a single day **/
+                conditions.Add(new ScanCondition("commence_time", ScanOperator.BeginsWith, beginDate));
+            }
+
+            return conditions;
+
+
+        }
+
+        [HttpGet]
+        [Route("date", Name = "GetGamesByDate")]
+        public async Task<IEnumerable<GameScore>> GetByDate(string begin, string? end = null)
+        {
+            IEnumerable<ScanCondition> conditions = _getConditions(begin, end);
+            DynamoDBOperationConfig config = new DynamoDBOperationConfig()
+            {
+                ConditionalOperator = ConditionalOperatorValues.And
+            };
+            var scan = await _dbContext.ScanAsync<GameScore>(conditions, config).GetRemainingAsync();
             return scan;
         }
 
