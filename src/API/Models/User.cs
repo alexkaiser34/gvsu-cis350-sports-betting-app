25a79b1cbacde7485ca66cef9db5c6d97bb0947f
Alex Kaiser <kaiseale@mail.gvsu.edu>
2023-10-30 18:49:24 -0400

    Adds the user model and database service for a user

---
+++ src/API/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    [DynamoDBTable("Users")]
+    public class User : UserData
+    {
+        public string id { get; set; }
+
+        
+
+        // TODO: Add model for a wager and keep a list of wagers with each user
+        // May be best to keep a seperate table
+    }
+
+    public class UserData
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+    }
+
+}
