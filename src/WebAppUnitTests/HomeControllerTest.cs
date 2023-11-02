0334c287dd9881ad113652414d9f20cc095adcf2
alexkaiser34 <kaiseale@mail.gvsu.edu>
2023-11-02 13:26:31 -0400

    Updates unit test to fix github workflow

--- src/WebAppUnitTests/HomeControllerTest.cs
+++ src/WebAppUnitTests/HomeControllerTest.cs
@@ -2,17 +2,19 @@ using Microsoft.Extensions.Logging;
 using sports_betting_app.Controllers;
 using Moq;
 using Microsoft.AspNetCore.Mvc;
+using sports_betting_app.Data;
+using sports_betting_app.Models;
 
 namespace WebAppUnitTests
 {
     public class HomeControllerTest
     {
         HomeController controller;
-        private readonly ILogger<HomeController> _logger;
+        private readonly IAPIClientService<GameOdd> _api;
 
         public HomeControllerTest()
         {
-            controller = new HomeController(_logger);
+            controller = new HomeController(_api);
         }
 
         // Simple unit test to ensure a string is passed from controller to model
