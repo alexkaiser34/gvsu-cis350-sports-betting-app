ea3cfb99bc79763dc40689c86d94c60714cf24ef
Alex Kaiser <kaiseale@mail.gvsu.edu>
2023-11-03 18:56:46 -0400

    Adds wager helper to automatically updated wages on games that have completed

--- src/Backend/Services/App.cs
+++ src/Backend/Services/App.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Backend.Models;
+using System;
 
 namespace Backend.Services
 {
@@ -6,12 +7,15 @@ namespace Backend.Services
     {
         private readonly DbHelper _dbHelper;
         private readonly ApiHelper _apiHelper;
+        private readonly WagerHelper _wagerHelper;
+
 
         public App()
         {
             var jsonHelper = new JsonHelper();
             _dbHelper = new DbHelper(jsonHelper.getAwsKeys());
             _apiHelper = new ApiHelper(jsonHelper.getApiKey());
+            _wagerHelper = new WagerHelper();
         }
 
         public async Task updateScores()
@@ -37,9 +41,32 @@ namespace Backend.Services
                 await _dbHelper.Post(odds);
 
             }
-            catch {
+            catch 
+            {
                 Console.WriteLine("Error updating game odds");
             }
+        }
+
+        public async Task updateWagers()
+        {
+            try
+            {
+                List<GameScore> scores = await _dbHelper.GetAll<GameScore>();
+                List<Wager> wagers = await _dbHelper.GetAll<Wager>();
+
+                List<Wager> activeWagers = wagers.FindAll(x => x.completed == false);
+                List<GameScore> completedGames = scores.FindAll(x => x.completed == true);
+
+                IEnumerable<Wager> updatedWagers = _wagerHelper.updateActiveWagers(activeWagers, completedGames);
+                await _dbHelper.Post(updatedWagers);
+
+            }
+            catch
+            {
+                Console.WriteLine("Error updating wagers");
+            }
+
+
         }
     }
 }
