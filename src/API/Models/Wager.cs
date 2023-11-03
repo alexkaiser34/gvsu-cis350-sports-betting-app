2704b4fc6def303d0f9c2d30cefd624c0a4c459d
Alex Kaiser <kaiseale@mail.gvsu.edu>
2023-11-03 13:32:00 -0400

    Adds wager model and controller to API

---
+++ src/API/Models/Wager.cs
@@ -0,0 +1,29 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace API.Models
+{
+
+    [DynamoDBTable("Wagers")]
+    public class Wager : WagerData
+    {
+        public string id { get; set; }
+    }
+
+    public class WagerData
+    {
+        [Required]
+        public string user_id { get; set; }
+
+        public string game_id { get; set; }
+
+        public string bet_type {  get; set; }
+        
+        public float wager_amount { get; set; }
+
+        public float amount_win { get; set; }
+
+        public bool didWagerWin { get; set; }
+    }
+}
