eb401ffca46a681e0ad32dc41ad863d7c0a95485
Alex Kaiser <kaiseale@mail.gvsu.edu>
2023-11-15 18:59:57 -0500

    Adds bootstrap offcanvas window for wager slip

--- src/sports-betting-app/Controllers/BetsController.cs
+++ src/sports-betting-app/Controllers/BetsController.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using sports_betting_app.Data;
 using sports_betting_app.Models;
+using System.Net;
 
 namespace sports_betting_app.Controllers
 {
@@ -58,21 +59,30 @@ namespace sports_betting_app.Controllers
         }
 
         [HttpPost]
-        public void updateWagerBet(BetData bet)
+        public IActionResult updateWagerBet(BetData bet)
         {
             // retrieve wager list as string
             var tmp = JsonConvert.DeserializeObject<WagerData>(
                 _contextAccessor.HttpContext.Session.GetString("wagerList")
             );
 
+
             // if successful, add bet clicked
             if (tmp != null)
             {
+                BetData exists = tmp.bet_data.FirstOrDefault(x => x.bet_type == bet.bet_type && x.name == bet.name && x.price == bet.price);
+                if (exists != null)
+                {
+                    return StatusCode(400, Json("Bet is already in wager"));
+                }
                 tmp.bet_data.Add(bet);
             }
 
             // save the variable as a string
             _contextAccessor.HttpContext.Session.SetString("wagerList", JsonConvert.SerializeObject(tmp));
+
+            return StatusCode(200);
+
         }
     }
 }
