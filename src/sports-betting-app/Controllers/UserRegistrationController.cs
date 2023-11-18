1a1c695f704f8eca64af01a0962badf9b3a1ab6d
Alex Kaiser <alexkaiser@me.com>
2023-11-17 20:35:02 -0500

    Adds login page if user already has an account.
    The user can log in instead of creating a new account if they
    already created one.

--- src/sports-betting-app/Controllers/UserRegistrationController.cs
+++ src/sports-betting-app/Controllers/UserRegistrationController.cs
@@ -15,10 +15,17 @@ namespace sports_betting_app.Controllers
             _api = api;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(UserData? userIn = null)
         {
-            UserData tmp = new UserData();
-            return View(tmp);
+            if (userIn == null)
+            {
+                UserData tmp = new UserData();
+                return View(tmp);
+            }
+            else
+            {
+                return View(userIn);
+            }
         }
 
         public IActionResult LogOut()
@@ -27,11 +34,73 @@ namespace sports_betting_app.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+        public IActionResult LogIn(UserData? userIn = null)
+        {
+            if (userIn == null)
+            {
+                UserData tmp = new UserData();
+                return View(tmp);
+            }
+            else
+            {
+                return View(userIn);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> logInExisting(UserData user)
+        {
+            try
+            {
+                // get the user added
+                var userList = await _api.GetAll("User");
+
+                var userFound = userList.FirstOrDefault(m =>
+                    m.UserName.Equals(user.UserName) &&
+                    m.Password.Equals(user.Password));
+
+                if (userFound == null)
+                {
+                    TempData["ErrorMessage"] = "Invalid username or password";
+                    return RedirectToAction("LogIn", "UserRegistration", user);
+
+                }
+
+                // create and store the cookie when they log in
+                var cookie = new CookieOptions();
+                cookie.Expires = DateTime.Now.AddDays(7);
+                cookie.Path = "/";
+                Response.Cookies.Append("sports-bet-user", JsonConvert.SerializeObject(userFound), cookie);
+
+
+                // redirect to home page
+                return RedirectToAction("Index", "Home");
+
+
+            }
+            catch (Exception ex)
+            {
+                // Log the exception or handle it appropriately
+                return StatusCode(500); // Internal server error
+            }
+        }
+
+
+
         [HttpPost]
         public async Task<IActionResult> Register(UserData user)
         {
             try
             {
+                var userValidate = await _api.GetAll("User");
+
+                var userExists = userValidate.Find(m => m.UserName.Equals(user.UserName));
+
+                if (userExists != null)
+                {
+                    TempData["ErrorMessage"] = "User name already exists!";
+                    return RedirectToAction("Index", "UserRegistration", user);
+                }
                 // post the user to the api
                 await _api.AddByType<UserData>(user, "User/Post");
 
@@ -49,8 +118,9 @@ namespace sports_betting_app.Controllers
                 Response.Cookies.Append("sports-bet-user", JsonConvert.SerializeObject(userAdded), cookie);
 
 
-                // redirect to previous page
-                return Redirect(Request.Headers["Referer"].ToString());
+                // redirect to home page
+                return RedirectToAction("Index", "Home");
+
 
             }
             catch (Exception ex)
