3b6653167f700c0a748883049c93aa1b1337db0e
Alex Kaiser <alexkaiser@me.com>
2023-11-29 17:10:21 -0500

    Removes home test

--- src/WebAppUnitTests/HomeControllerTest.cs
+++ src/WebAppUnitTests/HomeControllerTest.cs
@@ -16,18 +16,5 @@ namespace WebAppUnitTests
         {
             controller = new HomeController(_api);
         }
-
-        // Simple unit test to ensure a string is passed from controller to model
-        [Fact]
-        public void Index()
-        {
-            var result = controller.Index() as ViewResult;
-            Assert.NotNull(result); 
-            var model = result.Model as String;
-            Assert.NotNull(model);
-            Assert.Equal("this is some test data", model);
-
-
-        }
     }
 }
