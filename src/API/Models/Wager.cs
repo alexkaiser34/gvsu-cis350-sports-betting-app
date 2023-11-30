c6eee964151f04435e572f951915ec6cda7b9a2c
Alex Kaiser <alexkaiser@me.com>
2023-11-30 18:41:59 -0500

    Updates wager model in frontend and backend

--- src/API/Models/Wager.cs
+++ src/API/Models/Wager.cs
@@ -44,5 +44,8 @@ namespace API.Models
         public string name { get; set; }
         public float price { get; set; }
         public float? point { get; set; }
+
+        public string gameTitle { get; set; }
+
     }
 }
